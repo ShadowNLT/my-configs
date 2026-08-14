481c05683e0df158dc0f2462d997abd948a5dd33
BatmanNLT <bouts-forked.0i@icloud.com>
2026-08-14 19:57:11 +0200

    nvim: show 4 parent dirs in lspsaga winbar breadcrumb (folder_level)

--- nvim/lua/BatmanNLT/plugins/lspsaga.lua
+++ nvim/lua/BatmanNLT/plugins/lspsaga.lua
@@ -8,6 +8,12 @@ return {
 				border = "rounded",
 				winblend = 0,
 			},
+			-- Winbar breadcrumb: file path + code symbols.
+			-- folder_level = how many parent dirs to show before the filename.
+			symbol_in_winbar = {
+				enable = true,
+				folder_level = 4,
+			},
 			lightbulb = {
 				enable = true,
 				sign = true,
